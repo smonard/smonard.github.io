f992d7864a7b55efa6ef4badceb215d9ae9bb846
Jose Silva Monard <josesilvamonard@gmail.com>
2019-10-15 17:57:12 -0500

    First commit

---
+++ assets/main.js
@@ -0,0 +1,19 @@
+const messages = [
+  "Espero que estés en el mejor día de tu vida", 
+  "I hope you are having the best day of your life",
+  "Надеюсь, у тебя лучший день в вашей жизни",
+  "Espero que você esteja no melhor dia da sua vida",
+  "J'espère que tu as le meilleur jour de ta vie"
+];
+
+var nextMessage = 0;
+
+function switchMessage() {
+  document.getElementById("message").innerHTML = messages[nextMessage];
+  nextMessage = nextMessage === 4 ? 0 : nextMessage + 1
+}
+
+function playMessages() {
+  switchMessage();
+  setInterval(switchMessage, 3300);
+}
